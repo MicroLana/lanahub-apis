fe1c9477525668ccb94cd7ad63ee0cdb7de31624
MicroLana <admin@microlana.co.zw>
2025-08-10 20:29:19 +0200

    updated ignore file

--- src/main/java/org/lanahub/lanahub/services/FirebaseInitializer.java
+++ src/main/java/org/lanahub/lanahub/services/FirebaseInitializer.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -14,15 +15,16 @@ public class FirebaseInitializer {
 
     @PostConstruct
     public void init() throws IOException {
-        InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase-service-account.json");
+        InputStream serviceAccount =
+                getClass().getClassLoader().getResourceAsStream("firebase-service-account.json");
 
         if (serviceAccount == null) {
             throw new RuntimeException("firebase-service-account.json not found in classpath");
         }
-
+        
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://lana-74bea-default-rtdb.firebaseio.com/")
+                .setDatabaseUrl("https://lanahub-f3079-default-rtdb.firebaseio.com/")
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
